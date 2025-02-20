8be9eb50b3a4c771ce3cc7b94ca54458ec64a4a1
Jacob-Beeson <118127404+Jacob-Beeson@users.noreply.github.com>
2025-02-20 12:52:39 +0000

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -13,10 +13,14 @@ def endpoint():
     return "",400 # Bad Request
 
 def calculate(n):
-  if n <= 0:
-    return 1
-  else:
-    return n * calculate(n-1)
+    if n <= 0:
+        return 1
+    result = 1
+    while n > 0:
+        result *= n
+        n -= 1
+    
+    return result
 
 if __name__ == "__main__":
   app.run(host="localhost",port=3000)
